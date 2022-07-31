39a8125e97a82f8a6c19ed396d8a94e5f6bd5206
Mikhail <mixailshurov@gmail.com>
2022-07-31 15:25:02 +0300

    release

--- showID.py
+++ showID.py
@@ -1,10 +1,23 @@
-import telebot
+# import telebot
+#
+# bot = telebot.TeleBot('5555283909:AAEx05bAX-qQC-z_ebgDLLPPKN1xVdvrpL4')
+#
+#
+# @bot.message_handler(content_types=["text"])
+# def show_id(message):
+#     print(message.chat.id)
+#
+# bot.infinity_polling()
+import asyncio
+from time import sleep
 
-bot = telebot.TeleBot('5555283909:AAEx05bAX-qQC-z_ebgDLLPPKN1xVdvrpL4')
 
+async def print_num(num):
+    while True:
+        print(num)
+        await asyncio.sleep(2)
 
-@bot.message_handler(content_types=["text"])
-def show_id(message):
-    print(message.chat.id)
-
-bot.infinity_polling()
+async def main():
+    await asyncio.run(print_num(5))
+    await asyncio.run(print_num(6))
+asyncio.run(main())
